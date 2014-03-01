699ba5d6a9dfbed92cc713e46c00e9a549d5fe62
Almaz <muktaalmaz@yandex.ru>
2014-03-01 19:21:41 +0400

    1

--- Client_Chat/src/main/java/client/Client.java
+++ Client_Chat/src/main/java/client/Client.java
@@ -1,15 +1,14 @@
 package client;
 /**
  * Этот класс отвечает за запуск клиента
- * 
  * Здесь возможен запуск клиентской части написанной 
- * на Swing, AWT или на других технологиях.
+ * Swing, AWT или на других технологиях.
  * 
  * @author Almaz
- 
  */
+
 public class Client {
     public static void main(String[] args) {
-        // TODO: Start client instance
+        // TODO: Start Client instance
     }
 }
--- Server_Chat/src/main/java/server/package-info.java
+++ Server_Chat/src/main/java/server/package-info.java
@@ -1,5 +1,8 @@
 /**
+ * 
+ */
+/**
  * @author Almaz
- *
+ * 
  */
 package server;
