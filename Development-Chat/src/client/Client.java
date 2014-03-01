83d21aa71672a0db67800f20678da3c622457d2b
Almaz <muktaalmaz@yandex.ru>
2014-03-01 18:20:55 +0400

    Добавлены два класса хелпера - Server | Client Starters

---
+++ Development-Chat/src/client/Client.java
@@ -0,0 +1,14 @@
+package client;
+/**
+ * Этот класс отвечает за запуск клиента
+ * 
+ * Здесь возможен запуск клиентской части написанной 
+ * на Swing, AWT или на других технологиях.
+ * 
+ * @author Almaz
+ */
+public class Client {
+    public static void main(String[] args) {
+        // TODO: Start client instance
+    }
+}
---
+++ Development-Chat/src/server/Server.java
@@ -0,0 +1,18 @@
+package server;
+
+/**
+ * Этот класс является запускающим сервер.
+ * Для запуска сервера необходимо вызвать метод
+ * main(String[] args) этого класса
+ * 
+ * @author Almaz
+ */
+
+public class Server {
+    public static void main(String[] args) {
+        // TODO: Start server instance
+        
+        
+        
+    }
+}
